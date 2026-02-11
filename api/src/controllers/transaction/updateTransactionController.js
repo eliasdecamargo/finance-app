e907a0e007480bd10372e07e0fd54ce98a664c2b
eliascamargo <eliasdecamargo99@gmail.com>
2026-02-11 20:22:51 -0300

    fix: pass correct params to UpdateTransactionRepository

---
+++ api/src/controllers/transaction/updateTransactionController.js
@@ -0,0 +1,5 @@
+export class UpdateTransactionController {
+    async execute() {
+        
+    }
+}
--- api/src/repositories/postgres/transaction/updateTransaction.js
+++ api/src/repositories/postgres/transaction/updateTransaction.js
@@ -1,29 +1,29 @@
-import { PostgresHelper } from "../../../database/postgres/helper.js"
+import { PostgresHelper } from '../../../database/postgres/helper.js'
 
 export class PostgresUpdateTransactionRepository {
-    async execute(userId, updateTransactionParams) {
-            const updateFields = []
-            const updateValues = []
-    
-            Object.keys(updateTransactionParams).forEach((key) => {
-                updateFields.push(`${key} = $${updateValues.length + 1}`)
-                updateValues.push(updateTransactionParams[key])
-            })
-    
-            updateValues.push(userId)
-    
-            const updateQuery = `
+    async execute(transactionId, updateTransactionParams) {
+        const updateFields = []
+        const updateValues = []
+
+        Object.keys(updateTransactionParams).forEach((key) => {
+            updateFields.push(`${key} = $${updateValues.length + 1}`)
+            updateValues.push(updateTransactionParams[key])
+        })
+
+        updateValues.push(transactionId)
+
+        const updateQuery = `
           UPDATE users
           SET ${updateFields.join(', ')}
           WHERE id = $${updateValues.length}
           RETURNING *
         `
-    
-            const updatedUser = await PostgresHelper.query(
-                updateQuery,
-                updateValues,
-            )
-    
-            return updatedUser[0]
-        }
+
+        const updatedUser = await PostgresHelper.query(
+            updateQuery,
+            updateValues,
+        )
+
+        return updatedUser[0]
+    }
 }
