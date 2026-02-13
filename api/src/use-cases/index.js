0a5af620128c4578a86a223698c2acbd87101242
eliascamargo <eliasdecamargo99@gmail.com>
2026-02-13 10:28:41 -0300

    feat: add GetUserBalanceUseCase

--- api/src/use-cases/index.js
+++ api/src/use-cases/index.js
@@ -2,6 +2,7 @@ export * from './user/create-user.js'
 export * from './user/getUserById.js'
 export * from './user/updateUser.js'
 export * from './user/deleteUser.js'
+export * from './user/getUserBalance.js'
 
 export * from './transaction/createTransaction.js'
 export * from './transaction/getTransactionsByUserId.js'
