63049b6616bd8d2dbcbed8314c81d31949dbac42
eliascamargo <eliasdecamargo99@gmail.com>
2026-02-13 10:39:41 -0300

    feat: add GetUserBalanceController

--- api/src/controllers/index.js
+++ api/src/controllers/index.js
@@ -2,6 +2,7 @@ export * from './user/create-user.js'
 export * from './user/getUserById.js'
 export * from './user/updateUser.js'
 export * from './user/deleteUser.js'
+export * from './user/getUserBalance.js'
 
 export * from './helpers/http.js'
 export * from './helpers/transaction.js'
---
+++ api/src/controllers/user/getUserBalance.js
@@ -0,0 +1,36 @@
+import { UserNotFoundError } from '../../errors/user'
+import {
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+    serverError,
+} from '../helpers.js'
+
+export class GetUserBalanceController {
+    constructor(getUserBalanceUseCase) {
+        this.getUserBalanceUseCase = getUserBalanceUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId
+
+            const idIsValid = checkIfIdIsValid(userId)
+
+            if (!idIsValid) {
+                return invalidIdResponse()
+            }
+
+            const balance = await this.getUserBalanceUseCase.execute({ userId })
+
+            return ok(balance)
+        } catch (error) {
+            if (error instanceof UserNotFoundError) {
+                return UserNotFoundError()
+            }
+
+            console.error(error)
+            return serverError()
+        }
+    }
+}
